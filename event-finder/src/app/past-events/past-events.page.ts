6e0a97e09179c86cc95d8e8f38be20ab1f59fdf8
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2025-03-11 14:25:13 +0000

    Add home, past events, upcoming events, and profile pages with navbar routing.

--- event-finder/src/app/past-events/past-events.page.ts
+++ event-finder/src/app/past-events/past-events.page.ts
@@ -3,12 +3,12 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
+  selector: 'app-past-events',
+  templateUrl: 'past-events.page.html',
+  styleUrls: ['past-events.page.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
 })
-export class Tab2Page {
+export class PastEventsPage {
 
   constructor() {}
 
--- event-finder/src/app/tabs/tabs.routes.ts
+++ event-finder/src/app/tabs/tabs.routes.ts
@@ -7,30 +7,35 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'home',
         loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+          import('../home/home.page').then((m) => m.HomePage),
       },
       {
-        path: 'tab2',
+        path: 'upcoming-events',
         loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+          import('../upcoming-events/upcoming-events.page').then((m) => m.UpcomingEventsPage),
       },
       {
-        path: 'tab3',
+        path: 'past-events',
         loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+          import('../past-events/past-events.page').then((m) => m.PastEventsPage),
+      },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('../profile/profile.page').then((m) => m.ProfilePage),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full',
   },
 ];
