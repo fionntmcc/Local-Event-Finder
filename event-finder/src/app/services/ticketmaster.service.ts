aa97a35bc3e762c870b52d35dd4bd6c2f0a36590
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2025-03-11 18:58:28 +0000

    Inject HttpClient into TicketmasterService and implement getEvents method to fetch events from Ticketmaster API with test url

--- event-finder/src/app/services/ticketmaster.service.ts
+++ event-finder/src/app/services/ticketmaster.service.ts
@@ -5,10 +5,20 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ApiResult } from './interfaces';
 
+const API_KEY = environment.ticketmaster_api_key;
+const API_URL = environment.ticketmaster_api_url;
+
 @Injectable({
   providedIn: 'root'
 })
 export class TicketmasterService {
-
+  // inject HttpClient
+  private httpClient = inject(HttpClient);
+  
   constructor() { }
+
+  // return top movies on given page
+  getEvents(page = 1): Observable<ApiResult> {
+    return this.httpClient.get<ApiResult>(`${API_URL}?countryCode=US&api_key=${API_KEY}`);
+  }
 }
