a0201c9e9ac08b4ac7fe525264162c2b81aec49a
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2025-03-11 15:37:49 +0000

    Add help popover to home page with app information and usage instructions

--- event-finder/src/app/home/home.page.ts
+++ event-finder/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonList, IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { IonList, IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonPopover, } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
@@ -17,7 +17,9 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
     IonCardHeader,
     IonCardTitle,
     IonCardContent,
-    ExploreContainerComponent
+    ExploreContainerComponent,
+    IonButton,
+    IonPopover,
   ],
 })
 
@@ -43,14 +45,14 @@ export class HomePage {
         id: '2', 
         title: 'Art Exhibition', 
         date: '2025-03-20', 
-        location: 'Art Gallery', 
+        location: 'Galway City Museum', 
         imageUrl: 'https://www.bing.com/th?id=OIP.6jl4Q0R_r19--rksPIadlQHaFR&w=176&h=185&c=8&rs=1&qlt=90&o=6&dpr=1.1&pid=3.1&rm=2' 
       },
       { 
         id: '3', 
         title: 'Food Festival', 
         date: '2025-03-25', 
-        location: 'Downtown', 
+        location: 'Eyre Square', 
         imageUrl: 'https://th.bing.com/th?id=OIP.tvALJOlQexfmeBPRV5jr2AHaE8&w=200&h=200&c=10&o=6&dpr=1.1&pid=La+gastronomie+africaine+fait+son+festival&rm=2' 
       },
     ];
