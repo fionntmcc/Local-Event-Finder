eca7a1c55ab50b5d4006fd6dc03421af23a737c1
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2025-03-12 23:07:50 +0000

    Event details page displays individual event details.

--- event-finder/src/app/event-details/event-details.component.ts
+++ event-finder/src/app/event-details/event-details.component.ts
@@ -1,104 +1,94 @@
 // All necessary imports
 import { Component, Input, OnInit, WritableSignal, inject, signal } from '@angular/core';
-import { RouterLinkWithHref } from '@angular/router';
-import { IonContent,
-   IonHeader, 
-   IonTitle, 
-   IonToolbar, 
-   IonIcon, 
-   IonCard, 
-   IonCardHeader, 
-   IonCardTitle, 
-   IonCardSubtitle, 
-   IonCardContent, 
-   IonText, 
-   IonLabel, 
-   IonButtons, 
-   IonButton, 
-   IonBackButton, 
-   IonItem, 
-   IonNav, 
-   IonAvatar,
-   IonToggle,
-   IonPopover,
-   IonInput,
-   IonRadio,
-   IonBadge,
-   } from '@ionic/angular/standalone';
+import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
+import { RouterLinkWithHref, ActivatedRoute } from '@angular/router';
+import { 
+  IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonCard, IonCardHeader,
+  IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonLabel, IonButtons,
+  IonButton, IonBackButton, IonItem, IonNav, IonAvatar, IonToggle, IonPopover,
+  IonInput, IonRadio, IonBadge, IonSpinner
+} from '@ionic/angular/standalone';
 import { PredictHqService } from '../services/predict-hq/predict-hq.service';
 import { StorageService } from '../services/storage/storage.service';
+import { LocationService } from '../services/location/location.service';
 import { ApiResult } from '../services/predict-hq/interfaces';
-import { CurrencyPipe, DatePipe} from '@angular/common';
 import { Browser } from '@capacitor/browser';
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  templateUrl: './details.page.html',
-  styleUrls: ['./details.page.scss'],
+  templateUrl: './event-details.component.html',
+  styleUrls: ['./event-details.component.scss'],
   standalone: true,
-  imports: [IonContent, 
-    IonHeader, 
-    IonTitle, 
-    IonToolbar, 
-    IonIcon, 
-    IonCard, 
-    IonCardHeader, 
-    IonCardTitle, 
-    IonCardSubtitle, 
-    IonCardContent, 
-    IonText, 
-    IonLabel, 
-    IonButtons, 
-    IonButton,
-    IonBackButton, 
-    IonItem, 
-    DatePipe, 
-    CurrencyPipe, 
-    IonNav, 
-    IonAvatar, 
-    RouterLinkWithHref,
-    IonToggle,
-    IonPopover,
-    IonInput,
-    IonRadio, 
-    FormsModule,
-    IonBadge,
+  imports: [
+    CommonModule,
+    IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonCard, IonCardHeader,
+    IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonLabel, IonButtons,
+    IonButton, IonBackButton, IonItem, DatePipe, CurrencyPipe, IonNav, IonAvatar,
+    RouterLinkWithHref, IonToggle, IonPopover, IonInput, IonRadio, FormsModule,
+    IonBadge, IonSpinner
   ]
 })
-
-
-
 export class EventDetailsPage implements OnInit {
   // inject services
   private predictHqService = inject(PredictHqService);
   private storageService = inject(StorageService);
+  private locationService = inject(LocationService);
+  private route = inject(ActivatedRoute);
 
   // Necessary inits
-  public imageBaseUrl = "https://image.tmdb.org/t/p";
-  public event:WritableSignal<Event | null> = signal(null);
-  public isPopupActive:boolean = false;
-  public homepage:string = "";
-  public status:string = "";
-  public eventId:string = "";
+  public event: any = null;
+  public isPopupActive: boolean = false;
+  public homepage: string = "";
+  public status: string = "";
+  public eventId: string = "";
 
-  @Input()
-  set id(id:string) {
-    this.predictHqService.getEventById(id).subscribe((event) => {
-      console.log(event);
-      this.event.set(event); 
-      this.homepage = event.homepage;
-      this.eventId = id;
+  constructor() {}
+
+  ngOnInit() {
+    // Get event ID from route parameters
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      if (id) {
+        this.eventId = id;
+        this.loadEventDetails(id);
+      }
+    });
+  }
+
+  loadEventDetails(id: string) {
+    this.predictHqService.getEventById(id).subscribe((res) => {
+      if (res && res.results && res.results.length > 0) {
+        this.event = res.results[0];
+        
+        // Set homepage URL if available
+        if (this.event.url) {
+          this.homepage = this.event.url;
+        }
+      }
     });
   }
 
   async openEventWebsite() {
-    await Browser.open({url: this.homepage});
+    if (this.homepage) {
+      await Browser.open({url: this.homepage});
+    } else if (this.event && this.event.url) {
+      await Browser.open({url: this.event.url});
+    }
   }
 
+  getDirections() {
+    if (!this.event || !this.event.location || !this.event.location.length) return;
+    
+    const lat = this.event.location[1];
+    const lng = this.event.location[0];
+    const url = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
+    
+    Browser.open({url});
+  }
 
-  constructor() {}
-
-  ngOnInit() {}
-
+  getDistance(): string {
+    if (!this.event || !this.event.location) return 'Distance unknown';
+    return this.locationService.getDistance(this.event.location[1], this.event.location[0]);
+  }
 }
