f498b228c61f6e12ecba80efda233900c4d199b9
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2025-03-12 21:25:25 +0000

    Added additional maxDistance param to event service for usability

--- event-finder/src/app/services/predict-hq/predict-hq.service.ts
+++ event-finder/src/app/services/predict-hq/predict-hq.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ApiResult, Event } from './interfaces';
+import { ApiResult } from './interfaces';
 
 const API_KEY = environment.event_finder_api_key;
 const API_URL = environment.event_finder_api_url;
@@ -18,13 +18,14 @@ export class PredictHqService {
     constructor() { }
 
     // return top movies on given page
-    getEvents(page = 1, latitude = 0, longitude = 0, query = "", date?: Date): Observable<ApiResult> {
-
+    getEvents(page = 1, latitude = 0, longitude = 0, query = "", date?: Date, maxDistance = 20): Observable<ApiResult> {
+        
+        // if no date, set furthest date for 3 months from now
         if (!date) {
             date = new Date();
-            date.setMonth(date.getMonth() + 1);
+            date.setMonth(date.getMonth() + 3);
         }
-        return this.httpClient.get<ApiResult>(`${API_URL}?within=10km@${latitude},${longitude}&active.lte=${date.toISOString().split('T')[0]}&q=${query}&page=${page}`,
+        return this.httpClient.get<ApiResult>(`${API_URL}?within=${maxDistance}km@${latitude},${longitude}&active.lte=${date.toISOString().split('T')[0]}&q=${query}&page=${page}`,
             {
                 headers: {
                     'Authorization': `Bearer ${API_KEY}`,
